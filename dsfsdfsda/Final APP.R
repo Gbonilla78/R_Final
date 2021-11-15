0fadc3ff7fd652a4f46e417866937a4239b6cbd1
unknown <gbonilla@vols.utk.edu>
2021-11-14 20:37:07 -0500

    Comit 4

--- dsfsdfsda/Final APP.R
+++ dsfsdfsda/Final APP.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(shinyjs)
+install.packages(shinyjs)
 
 Stress<-c(1,2,3,4,6,7,8,9,7,6,3,5,6,7,8)
 Time_Spent<-c(5,6,7,8,4,2,2,5,6,7,8,6,5,6,5)
@@ -12,6 +14,9 @@ ui <- fluidPage(
         sidebarPanel(
             helpText("This is a Drew Steen motivational survey"),
             actionButton("button", "Click me for R Stress levels"),
+            useShinyjs(),
+            actionLink("button", "Show additional"),
+            hidden(div(id='text_div', verbatimTextOutput("text"))),
         
             
             selectInput("var", 
@@ -27,6 +32,8 @@ ui <- fluidPage(
                         label = "Numeric level of stress",
                         min = 0, max = 100, value = 50)
         ),
+        
+        
         
         mainPanel(
             textOutput("selected_var"),
@@ -50,6 +57,36 @@ server <- function(input, output) {
         output$distPlot <- renderPlot({
             plot(data1, mapping = aes(x="Stress", y="Time_Spent") 
                    + geom_count())
+            server = function(input, output, session){
+                observeEvent(input$button, {
+                    toggle('text_div')
+                    output$text <- renderText({"Additional"})
+                    
+                    if (input$button %% 2 == 1) {
+                        txt <- "Hide Additional"
+                    } else {
+                        txt <- "Show Additional"
+                    }
+                    updateActionButton(session, "button", label = txt)
+                })
+                
+            }
+            
+            server = function(input, output, session){
+                observeEvent(input$button, {
+                    toggle('text_div')
+                    output$text <- renderText({"Additional"})
+                    
+                    if (input$button %% 2 == 1) {
+                        txt <- "Hide Additional"
+                    } else {
+                        txt <- "Show Additional"
+                    }
+                    updateActionButton(session, "button", label = txt)
+                })
+                
+            }
+            
             
             
         })
